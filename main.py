707d829d40a472bcc30b7bb4858825eb399a6a40
Oleksandr Masalykin <masalykin1983@gmail.com>
2022-04-12 16:19:50 +0300

    Add demo tanks

--- main.py
+++ main.py
@@ -3,11 +3,59 @@ from random import randint
 
 pygame.init()
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 800, 600
 FPS = 60
+TILE = 32
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+DIRECTIONS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+
+
+class Tank:
+    def __init__(self, color, px, py, direct, keyList):
+        objects.append(self)
+        self.type = 'tank'
+
+        self.color = color
+        self.rect = pygame.Rect(px, py, TILE, TILE)
+        self.direct = direct
+        self.moveSpeed = 2
+
+        self.keyLEFT = keyList[0]
+        self.keyRIGHT = keyList[1]
+        self.keyUP = keyList[2]
+        self.keyDOWN = keyList[3]
+        self.keySHOT = keyList[4]
+
+    def update(self):
+        if keys[self.keyLEFT]:
+            self.rect.x -= self.moveSpeed
+            self.direct = 3
+        elif keys[self.keyRIGHT]:
+            self.rect.x += self.moveSpeed
+            self.direct = 1
+        elif keys[self.keyUP]:
+            self.rect.y -= self.moveSpeed
+            self.direct = 0
+        elif keys[self.keyDOWN]:
+            self.rect.y += self.moveSpeed
+            self.direct = 2
+
+    def draw(self):
+        pygame.draw.rect(window, self.color, self.rect)
+
+        x = self.rect.centerx + DIRECTIONS[self.direct][0] * 30
+        y = self.rect.centery + DIRECTIONS[self.direct][1] * 30
+        pygame.draw.line(window, 'white', self.rect.center, (x, y), 4)
+
+
+objects = []
+Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d,
+     pygame.K_w, pygame.K_s, pygame.K_SPACE))
+Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT,
+     pygame.K_UP, pygame.K_DOWN, pygame.K_KP_ENTER))
 
 is_play = True
 
@@ -16,16 +64,14 @@ while is_play:
         if event.type == pygame.QUIT:
             is_play = False
 
-    red = randint(0, 255)
-    green = randint(0, 255)
-    blue = randint(0, 255)
-    rgb_collor = (red, green, blue)
-    coordinate_x = randint(0, WIDTH)
-    coordinate_y = randint(0, HEIGHT)
-    coordinates = (coordinate_x, coordinate_y)
-    rect_sizes = (10, 10)
+    keys = pygame.key.get_pressed()
 
-    pygame.draw.rect(window, rgb_collor, (*coordinates, *rect_sizes))
+    for obj in objects:
+        obj.update()
+
+    window.fill('black')
+    for obj in objects:
+        obj.draw()
 
     pygame.display.update()
     clock.tick(FPS)
